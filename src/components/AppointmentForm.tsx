0c26d8d768086b763461a719a315e076ce180526
Héte Caetano <43750256+hecasan@users.noreply.github.com>
2025-03-27 20:20:18 -0300

    atualiza README.md e adiciona tipos TypeScript para agendamentos e médicos; reorganiza importações e remove tipos duplicados

--- src/components/AppointmentForm.tsx
+++ src/components/AppointmentForm.tsx
@@ -3,7 +3,8 @@ import styled from 'styled-components/native';
 import { Button, Input, Text } from 'react-native-elements';
 import { Platform, View, TouchableOpacity } from 'react-native';
 import theme from '../styles/theme';
-import { Doctor } from '../types';
+import { Doctor } from '../types/doctors';
+import { Appointment } from '../types/appointments';
 
 const doctors: Doctor[] = [
    {
---
+++ src/types/navigation.ts
@@ -0,0 +1,16 @@
+/**
+ * Tipos relacionados à navegação
+ * Este arquivo contém todas as definições de tipos necessárias para a navegação entre telas
+ */
+
+/**
+ * Define as rotas disponíveis na aplicação e seus parâmetros
+ * @property Home - Tela inicial da aplicação
+ * @property CreateAppointment - Tela de criação de consulta
+ * @property Profile - Tela de perfil do usuário
+ */
+export type RootStackParamList = {
+  Home: undefined;
+  CreateAppointment: undefined;
+  Profile: undefined;
+}; 
